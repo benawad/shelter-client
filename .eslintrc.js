1f3fa7fbe99d3312d7bf6ae93b301c97631f003a
ben awad <benawad97@gmail.com>
2017-09-30 15:10:44 -0500

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default () => (
+  <View style={{ paddingTop: 100 }}>
+    <Text>hi</Text>
+  </View>
+);
