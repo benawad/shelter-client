b0999e43c6e102e47f03a5ceb9df217318b68801
ben awad <benawad97@gmail.com>
2017-09-30 18:42:13 -0500

    routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import { View } from 'react-native';
 import { NativeRouter, Route } from 'react-router-native';
 
 import Home from './Home';
-import CreateShelter from './CreateShelter';
 import GuestRegister from './GuestRegister';
+import DonorRegister from './DonorRegister';
+import CreateShelter from './CreateShelter';
+import ShelterDetails from './ShelterDetails';
 
 export default () => (
   <NativeRouter>
-    <Route exact path="/" component={Home} />
+    <View style={{ flex: 1 }}>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/GuestRegister" component={GuestRegister} />
+      <Route exact path="/DonorRegister" component={DonorRegister} />
+      <Route exact path="/CreateShelter" component={CreateShelter} />
+      <Route exact path="/ShelterDetails" component={ShelterDetails} />
+    </View>
   </NativeRouter>
 );
